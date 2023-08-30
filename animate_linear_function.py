842c3c98bfefb4684687810b57980daa21afb387
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-30 12:40:39 +0530

    add function body to animate_using_pause function.

--- animate_linear_function.py
+++ animate_linear_function.py
@@ -1,4 +1,14 @@
+from matplotlib import pyplot
 if __name__ == '__main__':
     ...
 def animate_using_pause():
-    ...
+    x = []
+    y = []
+    for i in range(100):
+        x.append(i)
+        y.append(i)
+        pyplot.xlim(0, 100)
+        pyplot.ylim(0, 100)
+        pyplot.plot(x, y, color="green")
+        pyplot.pause(0.01)
+    pyplot.show()
